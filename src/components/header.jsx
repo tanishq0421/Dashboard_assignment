db85a9256cdb4610766f448fa8da29463e8aa1c6
tanishq <tanishq.shukla.cd.che21@iitbhu.ac.in>
2025-01-05 19:57:11 +0530

    add margin to the left of the header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,7 +8,7 @@ import profile from "./../assets/user.png"
 export const Header = () => {
     return(
         <div className="flex flex-col sm:flex-row mt-2 p-2 justify-between items-center bg-blue-900 w-full">
-            <div className="flex items-center space-x-4 mb-2 sm:mb-0">
+            <div className="flex items-center space-x-4 mb-2 sm:mb-0 ml-4">
                 <img src={grid} alt="grid" className="w-6 h-6"/>
                 <div className="text-xl font-md text-white">Test</div>
             </div>
